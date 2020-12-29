e556c2fdd70c278e39e92ff2ce82cc50d4f816aa
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-28 19:53:48 -0500

    terminando usuario

--- src/app/usuarios/lista/lista.component.ts
+++ src/app/usuarios/lista/lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario.model';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -9,10 +10,11 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 })
 export class ListaComponent implements OnInit {
 
+  public usuarios: Usuario;
   public constructor(private readonly usuarioService: UsuarioService) { }
 
   public ngOnInit(): void {
-    this.usuarioService.getUsers().subscribe((resp) => console.log(resp) )
+    this.usuarioService.getUsers().subscribe((resp) => this.usuarios = resp );
   }
 
 }
