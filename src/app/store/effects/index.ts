6730ea186bd8e78ad1e5bfc06c99bcbc94792edc
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-29 14:56:07 -0500

    capturando usuario info

--- src/app/store/effects/index.ts
+++ src/app/store/effects/index.ts
@@ -1,3 +1,4 @@
+import { UsuarioEffects } from './usuario.effects';
 import { UsuariosEffects } from './usuarios.effects';
 
-export const EffectsArray: Array<any> = [ UsuariosEffects ];
+export const EffectsArray: Array<any> = [ UsuariosEffects, UsuarioEffects ];
--- src/app/usuarios/usuario/usuario.component.ts
+++ src/app/usuarios/usuario/usuario.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Usuario } from 'src/app/models';
 import { cargarUsuario } from 'src/app/store/actions';
+import { AppState } from 'src/app/store/app.reducer';
 
 @Component({
   selector: 'app-usuario',
@@ -11,11 +13,20 @@ import { cargarUsuario } from 'src/app/store/actions';
 })
 export class UsuarioComponent implements OnInit {
 
+  public usuario: Usuario;
   public constructor(private readonly router: ActivatedRoute,
-                     private readonly store: Store) { }
+                     private  store: Store<AppState>) { }
 
   public ngOnInit(): void {
+    this.getDataFromStore();
+    this.getInfoFromUrl();
+  }
+
+  private getInfoFromUrl() {
     this.router.params.subscribe(({id}) => this.store.dispatch(cargarUsuario(id)));
   }
 
+  private getDataFromStore() {
+    this.store.select('usuario').subscribe(({user}) =>  this.usuario = user)
+  }
 }
